92155f44ba3ece54044720dfa4773458e140906f
ElectroFeel <113974602+ElectroFeel@users.noreply.github.com>
2023-03-15 17:44:44 +0300

    Add files via upload

---
+++ spaceQuiz/cosmicQuest/script.js
@@ -0,0 +1,106 @@
+'use strict';
+
+
+// Block of Questions 1
+const quest1 = ['Какая страна запустила первого человека в космос?', 'СССР', 'Китай', 'Россия', 'Канада', 'США'];
+
+const quest2 = ['Как зовут первого человека в космосе?', 'Юрий Гагарин', 'Антон Васильев', 'Валентин терешков', 'Алексей Леонов', 'Юрий Болгарин'];
+
+const quest3 = ['Как называется первый искусственный  спутник Земли?', 'Спутник-1', 'Луна', 'Даша', 'Салют', 'Космос'];
+const quest4 = ['Какой учёный изобрёл ракету и считается отцом космонавтики?', 'Циолковский', 'Гагарин', 'Эйнштейн', 'Ньютон', 'Королёв'];
+const quest5 = ['Как зовут первую женщину-космонавта?', 'Валентина Терешкова', 'Галина Иванова', 'Лариса Купалкина', 'Анна Космонавтова', 'Лиза Солнцева'];
+const quest6 = ['В каком году был совершён первый полёт в космос?', '1961', '1901', '1861', '2001', '1951'];
+const quest7 = ['Какие животные ещё НЕ побывали в космосе?', 'Кроты и бегемоты', 'Собаки и обезьяны', 'Лягушки и улитки', 'Крысы и мыши', 'Перепела и рыбы'];
+const quest8 = ['В каком состоянии прибывают космонавты,когда находятся в космосе?', 'Невесомости', 'Обобщённости', 'страха', 'радости', 'усталости'];
+const quest9 = ['В какой день отмечается день космонавтики?', '12 апреля', '1 сентября', '10 марта', '31 декабря', '1 апреля'];
+const quest10 = ['Сколько раз Гагарин летал в космос?', '1', '108', '7', '64', '13'];
+const quest11 = ['Сколько планет в нашей Солнечной системе?', '8', '4', '15', '10', '2'];
+const quest12 = ['Какая самая большая планета в Солнечной системе?', 'Юпитер', 'Солнце', 'Марс', 'Сатурн', 'Земля'];
+const quest13 = ['Какой по счёту планетой от солнца является земля?', '3', '1', '8', '5', '11'];
+const quest14 = ['Какие планеты соседствуют с Землёй?', 'Марс и Венера', 'Луна и Солнце', 'Сатурн и Уран', 'Нептун и Меркурий', 'Плутон и Юпитер'];
+const quest15 = ['Солнце- это ...?', 'Звезда', 'Планета', 'Спутник', 'Материк', 'Главная планета'];
+const quest16 = ['Какая самая ближайшая звезда к Земле?', 'Солнце', 'Альфа Центавра', 'Сириус', 'Бета Центавра', 'Пятиконечная'];
+
+// Block of Questions 2
+
+const quest17 = ['Сколько естественных спутников у Земли?', '1', '3', '14', '28', '3.14'];
+const quest18 = ['Какой космический объект влияет на приливы и отливы?', 'Луна', 'Солнце', 'Юпитер', 'Марс', 'Черная дыра'];
+const quest19 = ['Как зовут первого человека на луне?', 'Нил Армстронг', 'Чарли Чаплин', 'Джейк Салли', 'Майк Дипперсон', 'Джек линкольн'];
+const quest20 = ['За сколько суток Луна проходит полный цикл смены фаз?', '29', '31', '365', '14', '7'];
+const quest21 = ['Какая сила держит нас на Земле?', 'Притяжения', 'Тяжести', 'Мышечная', 'Натяжения', 'Магнитная'];
+const quest22 = ['Почему чёрная дыра "чёрная"?', 'Из-за гравитационного поля,из которого не может вырваться даже свет', 'Из-за тёмной массы, находящейся в чёрной дыре', 'Из-за камер, которые не могут сфотографировать дыру на цветную камеру', 'Из-за того, что в ней всегда ночь', 'Из-за тёмной космической пыли,которая летает в ней'];
+const quest23 = ['Юпитер- это...?', 'Газовый гигант', 'Большой спутник', 'Гигапланета', 'Максипланета', 'Ничего из этого'];
+const quest24 = ['В какой галактике мы живём?', 'Млечный путь', 'Кофейная дорога', 'Далекой-далекой', 'Подсолнух', 'Андромеда'];
+const quest25 = ['Какая звезда горячее?', 'Синяя', 'Красная', 'Оранжевая', 'Желтая', 'Зеленая'];
+const quest26 = ['Какая звезда самая яркая в ночном небе?', 'Сириус', 'Солнце', 'Полярная', 'Том Круз', 'Дубхе'];
+const quest27 = ['Какой вид галактик действительно существует?', 'Спиральная', 'Кубическая', 'Кольцевая', 'Пельменная', 'Кристальная'];
+const quest28 = ['Какие созвездия НЕ видны в Северном полушарии?', 'Лупа и Насос', 'Большая и Малая медведицы', 'Цефей и Кассиопея', 'Гончие псы и Орион', 'Дева и Стрелец'];
+const quest29 = ['Какой телескоп самый мощный?', 'Хаббл', 'Сидорова', 'Франца', 'Левенчук', 'Шульца'];
+const quest30 = ['Какие единицы измерения используются для обозначения расстояния в космосе?', 'Световой год', 'Темный месяц', 'Километр', 'Метр', 'Нанометр'];
+const quest31 = ['Назовите расстояние от Земли до Луны', '400 000 км', '1 млн. км.', '1 световой год', '1000 остановок на трамвае', '500 аршинов'];
+const quest32 = ['На какой планете проходили съемки фильма "Марсианин"', 'Земля', 'Луна', 'Марс', 'Венера', 'Пандора'];
+
+
+// Block of DOM
+const setQuestion = document.querySelector('.question');
+const setAns1 = document.getElementById('ans1');
+const setAns2 = document.getElementById('ans2');
+const setAns3 = document.getElementById('ans3');
+const setAns4 = document.getElementById('ans4');
+const setAns5 = document.getElementById('ans5');
+
+
+// All Variables
+let questionNumber = 0;
+let trueAnswer = '';
+let arrQuests = [];
+
+
+// Functions _______________________________________________________
+function randomizeQuestions(shuffle = []) {
+    setQuestion.textContent = shuffle[0];
+    let answers = [shuffle[1], shuffle[2], shuffle[3], shuffle[4], shuffle[5]];
+    answers.sort(() => Math.random() - 0.5);
+    setAns1.textContent = answers[0];
+    setAns2.textContent = answers[1];
+    setAns3.textContent = answers[2];
+    setAns4.textContent = answers[3];
+    setAns5.textContent = answers[4];
+    trueAnswer = shuffle[1];
+};
+
+function checkAnswer(answer = '') {
+    if (trueAnswer == answer) {
+        document.getElementById(`part${questionNumber+1}`).classList.remove('hide');
+    };
+
+    if (questionNumber < 15) {
+        questionNumber++;
+        randomizeQuestions(arrQuests[questionNumber]);
+    };     
+};
+
+
+
+// Code Start Here__________________________________________________
+
+document.querySelector('.left-button').addEventListener('click', function () {
+    document.querySelector('.modal').style.display = 'none';
+    arrQuests = [quest1, quest2, quest3, quest4, quest5, quest6, quest7, quest8, quest9, quest10, quest11, quest12, quest13, quest14, quest15, quest16];
+    let buttonsArr = [setAns1, setAns2, setAns3, setAns4, setAns5];
+    randomizeQuestions(arrQuests[questionNumber]);
+    for (let i = 0; i < buttonsArr.length; i++) {
+    buttonsArr[i].addEventListener('click', function () {checkAnswer(buttonsArr[i].textContent)});
+    };
+});
+
+document.querySelector('.right-button').addEventListener('click', function () {
+    document.querySelector('.modal').style.display = 'none';    
+    arrQuests = [quest17, quest18, quest19, quest20, quest21, quest22, quest23, quest24, quest25, quest26, quest27, quest28, quest29, quest30, quest31, quest32];
+    randomizeQuestions(arrQuests[questionNumber]);
+    let buttonsArr = [setAns1, setAns2, setAns3, setAns4, setAns5];
+    for (let i = 0; i < buttonsArr.length; i++) {
+    buttonsArr[i].addEventListener('click', function () {checkAnswer(buttonsArr[i].textContent)});
+    };
+});
+
